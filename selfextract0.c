f4d165f253f7aec0fa32bb1a9859ad8fb5023740
Javier Llopis <javier@llopis.me>
2021-02-07 17:17:47 +0000

    Run setup program after unarchiving

--- selfextract0.c
+++ selfextract0.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdarg.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -11,9 +12,27 @@
 
 #define THIS_FILE_SIZE   0
 
+static char *fstring(const char *pattern, ...)
+{
+    char *newstring;
+    size_t len;
+    va_list arglst;
+
+    va_start(arglst, pattern);
+    len = vsnprintf(NULL, 0, pattern, arglst);
+    va_end(arglst);
+
+    newstring = malloc((len + 1) * sizeof(char));
+
+    va_start(arglst, pattern);
+    vsnprintf(newstring, len+1, pattern, arglst);
+    va_end(arglst);
+
+    return newstring;
+}
+
 static char * make_temp_dir()
 {
-    size_t len;
     pid_t pid = getpid();
     char *tmpdir = getenv("TMP");
     char *td;
@@ -22,10 +41,8 @@ static char * make_temp_dir()
     if (NULL == tmpdir) {
         tmpdir = "/tmp";
     }
-    len = snprintf(NULL, 0, "%s/selfex__%d", tmpdir, pid);
-    td = malloc((len + 1) * sizeof(char));
-    snprintf(td, len, "%s/selfex__%d", tmpdir, pid);
 
+    td = fstring("%s/selfex__%d", tmpdir, pid);
     err = mkdir(td, 0700);
     if (err < 0) {
         fprintf(stderr, "Couldn't create temporary directory.\n");
@@ -62,6 +79,20 @@ static int untar(int fd, char *tmpdir)
     return 0;
 }
 
+static int run_setup(char *tmpdir)
+{
+    int rc;
+    char *cmd = fstring("%s/setup %s", tmpdir, tmpdir);
+    fprintf(stdout, "running '%s'\n", cmd);
+    rc = system(cmd);
+    if (rc == -1) {
+        fprintf(stderr, "Couldn't run setup [%d] %s\n", errno, strerror(errno));
+    } else if (rc > 0) {
+        fprintf(stderr, "Command setup failed rc = %d\n", rc);
+    }
+    return rc;
+}
+
 static void cleanup(char *tmpdir)
 {
     fprintf(stdout, "Cleaning up %s\n", tmpdir);
@@ -75,25 +106,32 @@ int main(int argc, char *argv[])
 
     fprintf(stdout, "Extracting...\n");
     if (NULL == (tmpdir = make_temp_dir())) {
-        return 1;
+        rc = 100;
+        goto exit;
     }
 
     if ((fd = open(argv[0], O_RDONLY)) < 0) {
         fprintf(stderr, "Couldn't open '%s': [%d] %s\n", argv[0], errno, strerror(errno));
-        return 2;
+        rc = 101;
+        goto exit;
     }
 
     fprintf(stdout, "Setting position at %d\n", THIS_FILE_SIZE);
     if (lseek(fd, THIS_FILE_SIZE, SEEK_SET) < 0) {
         fprintf(stderr, "Error seeking [%d] %s\n", errno, strerror(errno));
-        return 3;
+        rc = 102;
+        goto exit;
     }
 
     if (untar(fd, tmpdir) < 0) {
-        return 4;
+        rc = 103;
+        goto exit;
     }
 
+    rc = run_setup(tmpdir);
+
+exit:
     cleanup(tmpdir);
 
-    return 0;
+    return rc;
 }
