7fafc1842e703a1645d8213d19ce8900fbab3383
Javier Llopis <javier@llopis.me>
2021-02-09 08:53:42 +0000

    Added cleanup routine.

--- mkarchive.py
+++ mkarchive.py
@@ -1,6 +1,7 @@
 """Create a self extracting executable
 """
 import argparse
+import os
 import pathlib
 import re
 import shutil
@@ -12,9 +13,10 @@ import tarfile
 HERE = pathlib.Path(__file__).parent
 DEFAULT_LIBTAR_A = "/usr/lib/x86_64-linux-gnu/libtar.a"
 DEFAULT_TAR_NAME = "_tmp.tar"
+DEFAULT_SELF_EXTRACTOR_NAME = "selfextract"
 SELF_EXTRACTOR_PATTERN = HERE / "selfextract0.c"
-SELF_EXTRACTOR_SRC = HERE / "selfextract.c"
-SELF_EXTRACTOR = HERE / "selfextract"
+SELF_EXTRACTOR_SRC = HERE / "selfextract1.c"
+SELF_EXTRACTOR = HERE / "selfextract1"
 CHUNKSIZE = 1024 * 1024
 SIZE_PATTERN = """
     ^\#define
@@ -24,27 +26,32 @@ SIZE_PATTERN = """
     \d+
 """
 
+
 def create_tar(filelst, tarname=DEFAULT_TAR_NAME):
     print(f"Creating tar '{tarname}'")
     with tarfile.open(tarname, "w", format=tarfile.GNU_FORMAT) as tar:
         for f in filelst:
             tar.add(f)
 
+
 # def create_tar1(filelst, tarname=DEFAULT_TAR_NAME):
-    # print(f"Creating tar '{tarname}'")
-    # p = subprocess.run(["tar", "-cvf", str(tarname), ] + filelst)
-    # if p.returncode:
-        # raise RuntimeError(f"Couldn't build archive '{tarname}'")
+# print(f"Creating tar '{tarname}'")
+# p = subprocess.run(["tar", "-cvf", str(tarname), ] + filelst)
+# if p.returncode:
+# raise RuntimeError(f"Couldn't build archive '{tarname}'")
 
 
 def edit_file_size(name, newsize):
     print(f"Editing size to {newsize}")
     with name.open() as fd:
         content = fd.read()
-    newcontent = re.sub(SIZE_PATTERN, f"#define THIS_FILE_SIZE  {newsize}",
-                        content,
-                        count=1,
-                        flags=re.MULTILINE | re.VERBOSE)
+    newcontent = re.sub(
+        SIZE_PATTERN,
+        f"#define THIS_FILE_SIZE  {newsize}",
+        content,
+        count=1,
+        flags=re.MULTILINE | re.VERBOSE,
+    )
     if newcontent == content:
         raise RuntimeError("Substitution pattern not matched!")
 
@@ -62,7 +69,9 @@ def make_executable(src, exename, libtar=None):
         # p = subprocess.run(["gcc", "-static", "-O3", "-o", str(exename), str(src), "-ltar" ])
         # p = subprocess.run(["gcc", "-g", "-o", str(exename), str(src), "/home/javier/tartest/lib/libtar.a"])
         if p.returncode:
-            raise RuntimeError(f"Couldn't build self extractor '{exename}' from '{src}'")
+            raise RuntimeError(
+                f"Couldn't build self extractor '{exename}' from '{src}'"
+            )
     else:
         raise NotImplementedError(f"Platform '{sys.platform}' not (yet) supported.")
 
@@ -84,7 +93,7 @@ def create_self_extractor(libtar):
     print(f"Recompiling {src}")
     make_executable(src, selfext, libtar)
 
-    if size != selfext.stat().st_size: # sanity check
+    if size != selfext.stat().st_size:  # sanity check
         raise RuntimeError("Size mismatch on output executable!")
 
     print(f"Size of {selfext} matches {size}")
@@ -108,16 +117,33 @@ def cat_exe_archive(name, archive, output):
     output.chmod(0o755)
 
 
+def cleanup(*files):
+    print("Cleaning up...")
+    for f in files:
+        print(f"  removing '{f}'")
+        os.remove(f)
+
+
 def parse_cmdline(argv):
     p = argparse.ArgumentParser(description=__doc__)
-    p.add_argument("--output", "-o", metavar="name", default="selfextract",
-                    help="Name of the output self-extractor")
+    p.add_argument(
+        "--output",
+        "-o",
+        metavar="name",
+        default="selfextract",
+        help="Name of the output self-extractor. Default "
+        f"'{DEFAULT_SELF_EXTRACTOR_NAME}'",
+    )
     # p.add_argument("--run", "-r", metavar="program",
-                    # help="Executable program or script to run after extraction")
-    p.add_argument("--libtar", "-l", metavar="libtar-location", default=DEFAULT_LIBTAR_A,
-                    help=f"Location of libtar.a for linking. Default '{DEFAULT_LIBTAR_A}'")
-    p.add_argument("file", nargs="+",
-                    help="Files and directories to archive")
+    # help="Executable program or script to run after extraction")
+    p.add_argument(
+        "--libtar",
+        "-l",
+        metavar="libtar-location",
+        default=DEFAULT_LIBTAR_A,
+        help=f"Location of libtar.a for linking. Default '{DEFAULT_LIBTAR_A}'",
+    )
+    p.add_argument("file", nargs="+", help="Files and directories to archive")
     return p.parse_args(argv)
 
 
@@ -127,11 +153,11 @@ def main(argv=None):
 
     options = parse_cmdline(argv)
 
-    tarname=pathlib.Path(DEFAULT_TAR_NAME)
+    tarname = pathlib.Path(DEFAULT_TAR_NAME)
     create_tar(options.file, tarname)
     tempextractor = create_self_extractor(options.libtar)
     cat_exe_archive(tempextractor, tarname, pathlib.Path(options.output))
-
+    cleanup(tempextractor, SELF_EXTRACTOR_SRC, tarname)
 
 
 if __name__ == "__main__":
