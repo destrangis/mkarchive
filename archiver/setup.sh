24528d9fd496eefff6c1cd74f2ffbb58103f0578
Javier Llopis <javier@llopis.me>
2022-04-24 20:30:39 +0100

    Check the tmpdir argument is not unspecified

--- archiver/setup.sh
+++ archiver/setup.sh
@@ -4,6 +4,12 @@
 # Simply copy the files that have just been extracted to a temporary
 # directory (which we get in $1) to the current directory.
 
+tmpdir=$1
+if [ -z "$tmpdir" ]; then
+    echo "Error: No tmpdir argument passed to setup"
+    exit 1
+fi
+
 echo "Copying files to current directory"
 for item in $1/*
 do
